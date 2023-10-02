53c2b28df34a08f90d14b0995e3c582878ffc908
NikitaBes4 <mandarin215@gmail.com>
2023-10-02 08:31:42 +0000

    02.10

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,56 @@
 <script>
-let EvenNum = []
-let src = 'https://upload.wikimedia.org/wikipedia/ru/6/61/Rickrolling.gif';
-let name = 'Rick Astley';
-function AddNum()
-	{
-			EvenNum = [...EvenNum, EvenNum.length + 1]	
+    let newItem = '';
+
+    let todoList = [{text: 'Write my first post', status: true},
+                    {text: 'Upload the post to the blog', status: false},
+                    {text: 'Publish the post at Facebook', status: false}];
+
+	$: uncompletedCount = todoList.filter(t => t.status).length;
+	$: allcount = todoList.length;
+	function addToList() {
+		todoList = [...todoList, {text: newItem, status: false}];
+		newItem = '';
 	}
+	
+	function removeFromList(index) {
+		todoList.splice(index, 1)
+		todoList = todoList;
+    }
 
-function DelNum(idx)
-{
-	EvenNum = EvenNum.filter((e,i)=> idx != i)
-}
+	function asksStatus() {
 
-$:  border = EvenNum.length
+    }
 </script>
-
-<button on:click={AddNum}>
-	<h1>AddNumer</h1>
-</button>
-
-<p>{EvenNum}</p>
-<div style = {`border:${border}px solid gray`}>
-	{#each EvenNum as num}
-	 {#if num%3 == 0 && num%3 == 0}
-	 <p class = "red">{num} </p>
-		{/if}
-		{#if num%2 == 0 && num%2 == 0}
-	<p class = "blue">{num} </p>
-	  {/if}
-	{:else}
-	<img {src} alt="{name} dances." />
-	{/each} 
+<div class = "flexclass">
+	<div>
+		<h1>Список дел</h1>
+		<input bind:value={newItem} type="text" placeholder="new todo item..">
+    <button on:click={addToList}>
+		Add
+	</button>
+		<br/>
+		{#each todoList as item, index}
+	<span on:click={() => removeFromList(index)}>❌</span>
+	<input bind:checked={item.status} type="checkbox">
+	<span class:checked={item.status}>{item.text}</span>
+	<br/>
+{/each} 
+<p>{uncompletedCount} из {allcount} дел выполнено</p>
+	</div>
 </div>
 
-<div>
-	
-    {#each EvenNum as num, i}
-	<button
-	on:click = {() => DelNum(i)}>
-		{num}</button>
+<style> 
+	.checked {
+    text-decoration: line-through;
+    }
 
-	{/each}
-
-</div>
+	.flexclass {
+ 	display: flex;
+ 	flex-wrap: wrap;
+ 	justify-content: center;
+ 	align-content: center;
+ 	height: 100vh;
+		
+  }
 	
-<style>
-	.even{background:blue;}
-	.odd{background:red;}
-</style>
+</style> 

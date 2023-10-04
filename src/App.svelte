e8d6dd001dffd1ea2488537bc65812c990eeb650
NikitaBes4 <mandarin215@gmail.com>
2023-10-04 06:46:49 +0000

    04.10

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
-    let newItem = '';
+    // import { prevent_default } from "svelte/internal";
+
+    let newItem = "";
 
     let todoList = [{text: 'Write my first post', status: true},
                     {text: 'Upload the post to the blog', status: false},
@@ -7,6 +9,7 @@
 
 	$: uncompletedCount = todoList.filter(t => t.status).length;
 	$: allcount = todoList.length;
+	
 	function addToList() {
 		todoList = [...todoList, {text: newItem, status: false}];
 		newItem = '';
@@ -16,41 +19,62 @@
 		todoList.splice(index, 1)
 		todoList = todoList;
     }
-
-	function asksStatus() {
-
-    }
 </script>
+
 <div class = "flexclass">
 	<div>
-		<h1>Список дел</h1>
-		<input bind:value={newItem} type="text" placeholder="new todo item..">
-    <button on:click={addToList}>
-		Add
-	</button>
-		<br/>
-		{#each todoList as item, index}
-	<span on:click={() => removeFromList(index)}>❌</span>
-	<input bind:checked={item.status} type="checkbox">
-	<span class:checked={item.status}>{item.text}</span>
-	<br/>
-{/each} 
+<h1>Список дел</h1>
+<form on:submit|preventDefault={addToList}>
+<input bind:value={newItem} type="text" placeholder="new todo item..">
+<button disabled={!newItem}>
+	Add
+</button>
+<!-- <button on:click={addToList}>
+ Add
+</button> -->
+</form>
+
+{#each todoList as item, index}
+
+<div class = "table">
+<input bind:checked={item.status} type="checkbox">
+<div class:checked={item.status}>
+	{item.text}
+</div>
+<div class = "cross" on:click={() => removeFromList(index)}>
+	&times;
+</div>
+</div>
+
+{/each}
 <p>{uncompletedCount} из {allcount} дел выполнено</p>
 	</div>
 </div>
 
 <style> 
-	.checked {
+	.checked 
+	{
     text-decoration: line-through;
     }
 
-	.flexclass {
+	.flexclass 
+	{
  	display: flex;
  	flex-wrap: wrap;
  	justify-content: center;
  	align-content: center;
- 	height: 100vh;
-		
+ 	height: 100vh;	
+  	}
+  
+  .item div:nth-child(2)
+  {
+  width: 100%;
   }
-	
+
+  .cross
+  {
+	font-size: 2em;
+	color: brown;
+  }
+
 </style> 

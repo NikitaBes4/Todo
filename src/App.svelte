78960687f7e97db13faa3e436d5aaf24a8852f3b
NikitaBes4 <mandarin215@gmail.com>
2023-10-10 06:01:33 +0000

    10.10

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-    // import { prevent_default } from "svelte/internal";
+    import TodoItem from "./TodoItem.svelte"
 
     let newItem = "";
 
@@ -14,44 +14,38 @@
 		todoList = [...todoList, {text: newItem, status: false}];
 		newItem = '';
 	}
-	
 	function removeFromList(index) {
 		todoList.splice(index, 1)
 		todoList = todoList;
     }
 </script>
 
-<div class = "flexclass">
-	<div>
+<div>
+<div>
 <h1>Список дел</h1>
 <form on:submit|preventDefault={addToList}>
 <input bind:value={newItem} type="text" placeholder="new todo item..">
 <button disabled={!newItem}>
 	Add
 </button>
-<!-- <button on:click={addToList}>
- Add
-</button> -->
 </form>
 
 {#each todoList as item, index}
 
-<div class = "table">
-<input bind:checked={item.status} type="checkbox">
-<div class:checked={item.status}>
-	{item.text}
-</div>
-<div class = "cross" on:click={() => removeFromList(index)}>
-	&times;
-</div>
-</div>
+<TodoItem 
+ItemData = {item}
+ItemIndex = {index} 
+Remove = {removeFromList} 
+
+/>
 
 {/each}
+
 <p>{uncompletedCount} из {allcount} дел выполнено</p>
-	</div>
+</div>
 </div>
 
-<style> 
+<style>
 	.checked 
 	{
     text-decoration: line-through;
@@ -65,16 +59,4 @@
  	align-content: center;
  	height: 100vh;	
   	}
-  
-  .item div:nth-child(2)
-  {
-  width: 100%;
-  }
-
-  .cross
-  {
-	font-size: 2em;
-	color: brown;
-  }
-
-</style> 
+</style>
---
+++ src/TodoItem.svelte
@@ -0,0 +1,33 @@
+<script>
+
+export let ItemData
+export let ItemIndex
+export let Remove
+export let Stats
+</script>
+
+<todoItem class = "flexclass">
+
+    <input bind:checked={ItemData.Stats} type="checkbox">
+    <div class:checked={ItemData.Stats}>
+        {ItemData.text}
+    </div>
+    <div class = "cross" on:click={() => Remove(ItemIndex)}>
+        &times;
+    </div>
+
+</todoItem>
+
+<style> 
+   .item div:nth-child(2)
+   {
+   width: 100%;
+   }
+
+   .cross
+   {
+   font-size: 2em;
+   color: brown;
+   }
+
+</style> 
